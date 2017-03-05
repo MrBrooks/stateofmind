318a6385f8ddf0ebf25ac9da7c6e5993860e456a
Denis Kulikov <altingfest@gmail.com>
2017-03-05 23:49:59 +0300

    initial commit

---
+++ src/js/custom.js
@@ -0,0 +1,10 @@
+/**************************************/
+/* Custom JavaScript files supervisor */
+/**************************************/
+
+$(window).load(function(){
+
+});
+$(document).ready(function() {
+
+});
---
+++ src/js/vendor.js
@@ -0,0 +1,16 @@
+/**************************************/
+/* Vendor JavaScript files supervisor */
+/**************************************/
+
+/* Third party */
+/*
+//= ../../bower_components/jquery/dist/jquery.js
+/* //= ../../bower_components/bootstrap/dist/js/bootstrap.js */
+//= ../../bower_components/owl.carousel/owl.carousel.js
+/* //= ../../bower_components/mask-jquery/dist/jquery.mask.js */
+/* //= ../../bower_components/underscore/underscore-min.js */
+/* //= ../../bower_components/Snap.svg/dist/snap.svg-min.js */
+/* //= ../../bower_components/ajaxSubmit/ajaxSubmit.js */
+//= ../../bower_components/parallax/deploy/jquery.parallax.min.js
+//= ../../bower_components/fabricjs/dist/fabric.js
+//= ../../bower_components/clipboard-js/dist/clipboard.min.js
