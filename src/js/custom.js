203c3ee92dc602f2545d4565b11eabcc7c791ac3
Denis Kulikov <altingfest@gmail.com>
2017-06-02 18:21:35 +0300

    add text styles, forms, button and menu-btn

--- src/js/custom.js
+++ src/js/custom.js
@@ -7,4 +7,44 @@ $(window).load(function(){
 });
 $(document).ready(function() {
 
+  var menu = new Menu();
+
 });
+
+
+function Menu(){
+  var menu_btn, state = 0;
+
+  function init(){
+    menu_btn = $('.menu-btn');
+
+    menu_btn.on('click', onClick);
+  }
+
+  function onClick(){
+    if(state == 0){
+      menuBtnOpen();
+    } else{
+      menuBtnClose();
+    }
+
+  }
+
+  function menuBtnOpen(){
+    menu_btn.addClass('open');
+    setTimeout(function(){
+      menu_btn.addClass('cross');
+      state = 1;
+    },500);
+  }
+
+  function menuBtnClose(){
+    menu_btn.removeClass('cross');
+    setTimeout(function(){
+      menu_btn.removeClass('open');
+      state = 0;
+    },500);
+  }
+
+  init();
+}
